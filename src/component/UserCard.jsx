cc1b5ab6da026168b96b88d3f4eebcf3d1ce88d2
Karan bhuva <karanbhuva9861@gmail.com>
2025-01-31 22:47:27 +0530

    Revert "Merge pull request #5 from Karanbhuva18/karan/redesign"
    This reverts commit 87da70e62192ee347d9c56533f069f3415add9f4, reversing
    changes made to 72b1deddc36ace6cce5e0b90084a0ec3fb4a7929.

--- src/component/UserCard.jsx
+++ src/component/UserCard.jsx
@@ -365,36 +365,33 @@ export default function UserCards() {
   ];
 
   return (
-    <div>
-      <h1 className="user-cards-title">employee daata List</h1>
-      <div className="user-cards-grid">
-        {users.map((user) => (
-          <div key={user.id} className="user-card">
-            <h2 className="user-card-title">{user.name}</h2>
-            <p className="user-card-id">ID: {user.id}</p>
-            <div className="user-card-details">
-              <p>
-                <span className="label">💰 Salary:</span> {user.salary}
-              </p>
-              <p>
-                <span className="label">🏙 City:</span> {user.city}
-              </p>
-              <p>
-                <span className="label">🌍 Country:</span> {user.country}
-              </p>
-              <p>
-                <span className="label">📞 Phone:</span> {user.phone}
-              </p>
-              <p>
-                <span className="label">🎓 Degree:</span> {user.degree}
-              </p>
-              <p>
-                <span className="label">🏫 College:</span> {user.college}
-              </p>
-            </div>
+    <div className="user-cards-grid">
+      {users.map((user) => (
+        <div key={user.id} className="user-card">
+          <h2 className="user-card-title">{user.name}</h2>
+          <p className="user-card-id">ID: {user.id}</p>
+          <div className="user-card-details">
+            <p>
+              <span className="label">💰 Salary:</span> {user.salary}
+            </p>
+            <p>
+              <span className="label">🏙 City:</span> {user.city}
+            </p>
+            <p>
+              <span className="label">🌍 Country:</span> {user.country}
+            </p>
+            <p>
+              <span className="label">📞 Phone:</span> {user.phone}
+            </p>
+            <p>
+              <span className="label">🎓 Degree:</span> {user.degree}
+            </p>
+            <p>
+              <span className="label">🏫 College:</span> {user.college}
+            </p>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 }
